fc09205701fc85fdf24a661479874964b445cb4d
kawanaka <kawanaka_s@tenso.com>
2019-02-21 09:58:46 +0900

    線形探索アルゴリズム作成
    - 配列は要素、添え字、要素数などの情報を持っている
    - 添え字を指定してやれば要素を取得する事ができる

--- lib/search.rb
+++ lib/search.rb
@@ -1,7 +1,29 @@
-class Array
-  attr_accessor :num
+require "pry"
 
-  def initialize(num="5")
-    @num = num
+class LinearSearch
+  attr_accessor :num, :target
+
+  def run(num, target)
+    # 引数numを配列にする
+    # numbersに代入
+    numbers = num.to_a
+    # 空の添え字を作成
+    index = 0
+    # binding.pry
+    # numbersとtargetが異なる間繰り返す
+    while numbers[index] != target do
+      # numbersとtargetが異なる時
+      if numbers[index] != target
+        # 添え字の数値を1増やす
+        index += 1
+      # ifここまで
+      end
+    # whileここまで
+    end
+  # 添え字を表示
+  p index  
+
+  # runメソッドここまで  
   end
 end
+ 
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,9 +1,13 @@
 require "spec_helper"
 require "search"
 
-describe Array do
-  it "5" do
-    array = Array.new
-    expect(array.num).to eq "5"
+describe LinearSearch do
+  describe "#run" do
+    context "numbersにtargetが含まれていた場合" do
+      it "見つかった時のtargetの添字を返す" do
+        liner_search = LinearSearch.new
+        expect(liner_search.run(1..10, 5)).to eq 4
+      end
+    end
   end
 end
