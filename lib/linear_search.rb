469e2214e91631d6136d2b916baf126e4f3c4266
kawanaka <kawanaka_s@tenso.com>
2019-02-22 15:16:40 +0900

    リファクタリング
    - 無駄にネストが深くなってしまったif文を修正
    - returnとbreakのそれぞれの違いについて理解した。

--- lib/linear_search.rb
+++ lib/linear_search.rb
@@ -6,36 +6,22 @@ class LinearSearch
   def run(target, check_array, index)
     # targetが数値かどうかを判別
     # targetが数値以外の場合
-    if target.kind_of?(Integer) != true
+    unless target.kind_of?(Integer)
       # '探索対象として正しくありません'を返す
-      '探索対象として正しくありません'
+      return '探索対象として正しくありません'
     # targetが数値の場合
-    else
-      # check_arrayとtargetが異なる間繰り返す
-      while check_array[index] != target
-        # 添え字の数値を1増やす
-        index += 1
-        # もし添え字と要素数が同じになった場合(arrayの中にtargetが含まれていない)
-        if index == check_array.size then
-          # 処理を抜ける
-          break
-        # if文ここまで
-        end
-      # whileここまで
-      end
-
-      # 添え字と要素数が同じ場合
-      if index == check_array.size
-        # '見つかりません'と返す
-        '見つかりません'
-      # 添え字と要素数が異なる場合 
-      else
-        # 添え字を返す
-        index
-      # ifここまで
-      end
-    # ifここまで  
     end
+
+    # check_arrayとtargetが異なる間繰り返す
+    while check_array[index] != target
+      # 添え字の数値を1増やす
+      index += 1
+      # 添え字と要素数が同じになった場合,処理を抜ける(arrayの中にtargetが含まれていない)
+      # '見つかりません'を返す
+      return '見つかりません'  if index == check_array.size 
+    # whileここまで
+    end
+    index
   # runメソッドここまで  
   end
 end
