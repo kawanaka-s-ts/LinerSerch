b79a36de1d16107915df5aa8759f2afbe128d59e
kawanaka <kawanaka_s@tenso.com>
2019-02-25 18:17:26 +0900

    戻り値を変更
    - 引数の名前を変更
    - raiseで意図的に例外を発生させる
    - 例外をテストする場合は、構文が異なる（{}を使う）

--- lib/linear_search.rb
+++ lib/linear_search.rb
@@ -1,29 +1,29 @@
 class LinearSearch
   # runメソッドここから
   # targetは探したい数値
-  # check_arrayは対象の配列
-  def run(target, check_array)
+  # numeric_arrayは調べる配列
+  def run(target, numeric_array)
     # targetが数値かどうかを判別
     # targetが数値以外の場合
     unless target.kind_of?(Integer)
-      # '探索対象として正しくありません'を返す
-      return '探索対象として正しくありません'
+      # 例外を返す
+      raise 'targetが数値ではありません'
     # targetが数値の場合
     end
 
     # indexに0を代入
     index = 0 
     # indexと要素数が異なっている間、繰り返しを行う
-    while index != check_array.size
+    while index < numeric_array.size
       # 配列内にtargetが見つかった場合、処理を終える
       # targetが見つかった時のindexを返す
-      return index if check_array[index] == target
+      return index if numeric_array[index] == target
       # indexの数値を1増やす
       index += 1
     # whileここまで
     end
-    # '見つかりません'を返す
-    '見つかりません'
+    # nilを返す
+    nil
   # runメソッドここまで  
   end
 end
--- spec/linear_search_spec.rb
+++ spec/linear_search_spec.rb
@@ -1,35 +1,35 @@
 require "spec_helper"
 require "linear_search"
 
-check_array = [1, 3, 7, 8, 5]
+numeric_array = [1, 3, 7, 8, 5]
 
 describe LinearSearch do
   describe "#run" do
-    context "check_arrayにtargetが含まれていた場合" do
+    context "numeric_arrayにtargetが含まれていた場合" do
       it "見つかった時のtargetの添え字を返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run(5, check_array)).to eq 4
+        expect(linear_search.run(5, numeric_array)).to eq 4
       end
     end
 
-    context "check_arrayにtargetが含まれていない場合" do
-      it "'見つかりません'を返す" do
+    context "numeric_arrayにtargetが含まれていない場合" do
+      it "nilを返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run(4, check_array)).to eq '見つかりません'
+        expect(linear_search.run(4, numeric_array)).to eq nil
       end
     end
 
     context "targetが数値以外の場合" do
-      it "'探索対象として正しくありません'を返す" do
+      it "例外を返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run("5", check_array)).to eq '探索対象として正しくありません'
+        expect{linear_search.run("5", numeric_array)}.to raise_error "targetが数値ではありません"
       end
     end
 
-    context "targetのindexが0の場合" do
+    context "targetの数値が配列の一番初めにある場合" do
       it "0を返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run(1, check_array)).to eq 0
+        expect(linear_search.run(1, numeric_array)).to eq 0
       end
     end
   end
