ac7268cc8b91d2780c1bc29ab585d40f84d8879b
kawanaka <kawanaka_s@tenso.com>
2019-02-25 11:44:17 +0900

    コメント修正

--- lib/linear_search.rb
+++ lib/linear_search.rb
@@ -3,15 +3,15 @@ class LinearSearch
   # targetは探したい数値
   # check_arrayは対象の配列
   def run(target, check_array)
-    # 添え字に0を代入
+    # indexに0を代入
     index = 0
     # check_arrayとtargetが異なる間繰り返す
     while check_array[index] != target
-      # 添え字の数値を1増やす
+      # indexの数値を1増やす
       index += 1
     # whileここまで
     end
-    # 繰り返しが終わった時の添え字を返す
+    # 繰り返しが終わった時のindexの値を返す
     index
   # runメソッドここまで  
   end
