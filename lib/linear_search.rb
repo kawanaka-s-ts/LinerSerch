e0ba7a3e475fdb62ab22552a36a838e774e7615c
kawanaka <kawanaka_s@tenso.com>
2019-02-26 14:06:04 +0900

    変数名変更

--- lib/linear_search.rb
+++ lib/linear_search.rb
@@ -1,8 +1,8 @@
 class LinearSearch
   # runメソッドここから
   # targetは探したい数値
-  # numeric_arrayは調べる配列
-  def run(target, numeric_array)
+  # numbersは調べる数値
+  def run(target, numbers)
     # targetが数値かどうかを判別
     # targetが数値以外の場合
     unless target.kind_of?(Integer)
@@ -12,12 +12,12 @@ class LinearSearch
     end
 
     # indexに0を代入
-    index = 0 
+    index = 0
     # indexと要素数が異なっている間、繰り返しを行う
-    while index < numeric_array.size
+    while index < numbers.size
       # 配列内にtargetが見つかった場合、処理を終える
       # targetが見つかった時のindexを返す
-      return index if numeric_array[index] == target
+      return index if numbers[index] == target
       # indexの数値を1増やす
       index += 1
     # whileここまで
--- spec/linear_search_spec.rb
+++ spec/linear_search_spec.rb
@@ -1,35 +1,35 @@
 require "spec_helper"
 require "linear_search"
 
-numeric_array = [1, 3, 7, 8, 5]
+numbers = [1, 3, 7, 8, 5]
 
 describe LinearSearch do
   describe "#run" do
-    context "numeric_arrayにtargetが含まれていた場合" do
+    context "numbersにtargetが含まれていた場合" do
       it "見つかった時のtargetの添え字を返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run(5, numeric_array)).to eq 4
+        expect(linear_search.run(5, numbers)).to eq 4
       end
     end
 
-    context "numeric_arrayにtargetが含まれていない場合" do
+    context "numbersにtargetが含まれていない場合" do
       it "nilを返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run(4, numeric_array)).to eq nil
+        expect(linear_search.run(4, numbers)).to eq nil
       end
     end
 
     context "targetが数値以外の場合" do
       it "例外を返す" do
         linear_search = LinearSearch.new
-        expect{linear_search.run("5", numeric_array)}.to raise_error "targetが数値ではありません"
+        expect{linear_search.run("5", numbers)}.to raise_error "targetが数値ではありません"
       end
     end
 
     context "targetの数値が配列の一番初めにある場合" do
       it "0を返す" do
         linear_search = LinearSearch.new
-        expect(linear_search.run(1, numeric_array)).to eq 0
+        expect(linear_search.run(1, numbers)).to eq 0
       end
     end
   end
